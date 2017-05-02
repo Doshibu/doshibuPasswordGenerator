fd80fd681a1b4e24a408e14df98f3c85a20fb5e3
Doshibu <guillaume.bonhommeau@viacesi.fr>
2017-05-03 00:51:16 +0200

    GeneratePassword

--- popup/popup.js
+++ popup/popup.js
@@ -10,14 +10,21 @@ $(document).ready(function(){
 	    snap: true
 	});
 
-	$('#btnCopy').click(function(){
-		var txt = $('#__password').val();
-		if(!txt || txt == ''){
-			return;
-		}
+	var password = GeneratePassword();
+	$('#__password').val(password);
+});
 
-		copyTextToClipboard(txt);
-	});
+$('#btnCopy').click(function(){
+	var txt = $('#__password').val();
+	if(!txt || txt == ''){
+		return;
+	}
+
+	copyTextToClipboard(txt);
+});
+
+$('#btnReset').click(function(){
+	GeneratePassword();
 });
 
 function copyTextToClipboard(text) {
@@ -29,4 +36,34 @@ function copyTextToClipboard(text) {
 	} catch (err) {
 		console.log('Doshibu Password Generator : Oops, unable to copy');
 	}
+}
+
+function GeneratePassword() {
+	var length = $('#__length').val(),
+		possibilities = '',
+		password = '';
+
+	if($('#checkLetters').is(':checked')) {
+		possibilities += 'qwertyuioplkjhgfdsazxcvbnmQWERTYUIOPLKJHGFDSAZXCVBNM';
+	}
+	if($('#checkNumbers').is(':checked')) {
+		possibilities += '1234567890';
+	}
+	if($('#checkSymbols').is(':checked')) {
+		possibilities += '!@#$%^&*.,';
+	}
+
+    for (i=0; i < length; i++) {
+        j = getRandomNum(possibilities.length);
+        password = password + possibilities.charAt(j);
+    }
+
+    return password;
+}
+
+function getRandomNum(cnt) {
+    // between 0 - 1
+    var rndNum = Math.random()
+    rndNum = parseInt(rndNum * cnt);
+    return rndNum;
 }
